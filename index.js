4ce10ddd8fed98feba23294f51e10ad32b7fe2de
aUnicornDev <86775297+aUnicornDev@users.noreply.github.com>
2021-09-18 00:31:56 +0530

    CU-8truc2 : Instagram images, OG, Contact Form

--- index.js
+++ index.js
@@ -119,33 +119,33 @@ const compareDuration = (duration,currDuration)=>{
     const formEmail = document.querySelector('#email');
     const formMessage = document.querySelector('#message');
 
-  //   formSubmit.addEventListener('click',e=>{
-  //     e.preventDefault();
-  //     let sheetBestBody = {
-  //       Name:formName.value,
-  //       Phone:formPhone.value,
-  //       Email:formEmail.value,
-  //       Message:formMessage.value
-  //     }
-  //     console.log(sheetBestBody);
-  //     fetch("https://sheet.best/api/sheets/9da63077-34f3-4696-a4d3-8cf3db3a142d", {
-  //     method: "POST",
-  //     mode: "cors",
-  //     headers: {
-  //         "Content-Type": "application/json",
-  //            },
-  //          body: JSON.stringify(sheetBestBody),
-  //     })
-  //     .then((r) => r.json())
-  //     .then((data) => {
-  //       // The response comes here
-  //       console.log(data);
-  //     })
-  //     .catch((error) => {
-  //       // Errors are reported there
-  //       console.log(error);
-  // });
-  //   })
+    formSubmit.addEventListener('click',e=>{
+      e.preventDefault();
+      let sheetBestBody = {
+        Name:formName.value,
+        Phone:formPhone.value,
+        Email:formEmail.value,
+        Message:formMessage.value
+      }
+      console.log(sheetBestBody);
+      fetch("https://sheet.best/api/sheets/9da63077-34f3-4696-a4d3-8cf3db3a142d", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+          "Content-Type": "application/json",
+             },
+           body: JSON.stringify(sheetBestBody),
+      })
+      .then((r) => r.json())
+      .then((data) => {
+        // The response comes here
+        console.log(data);
+      })
+      .catch((error) => {
+        // Errors are reported there
+        console.log(error);
+  });
+    })
 
 
 const loadMore = document.querySelector('.load-more');
@@ -176,4 +176,16 @@ showLess.addEventListener('click',()=>{
   loadMore.classList.remove('hide');
   showLess.classList.add('hide');
   document.getElementById("crew-achievements").scrollIntoView(true);
+})
+const contactUs = document.querySelector('.contact-us');
+const contactForm = document.querySelector('.contact-form');
+contactUs.addEventListener('click',()=>{
+  console.log('yfuftyfy')
+  if(contactForm.classList.contains('hide')){
+    contactForm.classList.remove('hide');
+  }
+  else{
+    contactForm.classList.add('hide');
+
+  }
 })
